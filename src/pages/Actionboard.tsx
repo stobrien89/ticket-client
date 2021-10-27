acc281751687952080b4aca2d86445162211372a
stobrien89 <stobrien@crimson.ua.edu>
2021-10-26 17:31:37 -0700

    add html to main page

--- src/pages/Actionboard.tsx
+++ src/pages/Actionboard.tsx
@@ -3,7 +3,23 @@ import React from 'react';
 
 export const Actionboard = ()  => {
     return(
-        <p>this is the body of the main page</p>
+        <div>
+            <div>
+                <h1>Actionboard</h1>
+                <button>settings</button>
+                <button>Admin</button>
+            </div>
+            <input type="text" placeholder="Search.."></input>
+            <div>
+                <button>tab1</button>
+                <button>tab2</button>
+                <button>tab3</button>
+            </div>
+            <div>
+                Tab Component
+            </div>
+        </div>
+
     )
 } 
 
